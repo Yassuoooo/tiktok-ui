0547f78bf7d6e3378170f39d3a3247f858a202e7
Eziio <thphong16902@gmail.com>
2024-07-25 18:25:35 +0700

    Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react'; // Fragment: component chứa ko sinh ra thẻ thật trong dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        // const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        // nếu ko có layout thì dùng Fragment, ngược lại lấy DefaultLayout
+
+                        // let Layout = DefaultLayout;
+
+                        // if ( route.layout === null ) {
+                        //     Layout = Fragment; // nếu ko có layout thì dùng Fragment
+                        // } else {
+                        //     layout = route.layout; // nếu có layout thì lấy layout đó
+                        // }
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout; // nếu có layout thì lấy layout đấy
+                        } else if (route.layout === null) {
+                            Layout = Fragment; // nếu ko có layout thì lấy Fragment chứa
+                        }
+
+                        const Page = route.component;
+                        // tạo JSX element Page tượng trưng cho route.component để dùng trong prop element
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
