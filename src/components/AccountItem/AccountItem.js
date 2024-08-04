91d8e8e41349cb2f03d6550ec40526baad4c1cca
Eziio <thphong16902@gmail.com>
2024-08-04 23:02:27 +0700

    Xây dựng Logic phần Header #2

--- src/components/AccountItem/AccountItem.js
+++ src/components/AccountItem/AccountItem.js
@@ -1,27 +1,30 @@
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { Image } from '~/components/Image';
 
 const cx = classNames.bind(styles); // bind styles vào function cx giúp css tiện hơn
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
             <Image
                 className={cx('avatar')}
-                src="https://www.sideshow.com/wp/wp-content/uploads/2022/09/Vergil-Closeup-Devil-May-Cry.jpg"
-                alt="name"
+                // src="https://www.sideshow.com/wp/wp-content/uploads/2022/09/Vergil-Closeup-Devil-May-Cry.jpg"
+                src={data.avatar}
+                alt={data.full_name}
             />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    Name
-                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                    {data.full_name}
+                    {data.tick && <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />}
+                    {/* chỉ hiển thị tick khi prop tick của object là true */}
                 </h4>
-                <span className={cx('user-name')}>scum</span>
+                <span className={cx('user-name')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { AccountItem as Account } from '~/components/AccountItem';
@@ -15,6 +15,7 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResults, setShowResults] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
 
@@ -24,7 +25,7 @@ function Search() {
 
     const handleClear = () => {
         setSearchValue('');
-        // setSearchResult([]);
+        setSearchResult([]);
         inputRef.current.focus();
     };
 
@@ -37,10 +38,26 @@ function Search() {
     };
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1]);
-        }, 0);
-    }, []);
+        // setTimeout(() => {
+        //     setSearchResult([1]);
+        // }, 0);
+        if (!searchValue.trim()) {
+            // xóa khoảng cách bằng trim nếu ko có searchValue
+            setSearchResult([]); // xóa mảng state searchResult nếu ko còn giá trị trên input
+            return;
+        }
+        setLoading(true); // trong lúc nhập input sẽ hiển thị icon loading
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`) // fetch api get
+            .then((res) => res.json()) // chuyển respone sang json
+            .then((res) => {
+                setSearchResult(res.data); // dùng setSearchResult đẩy data trong respone vào [] của state searchResult
+                setLoading(false); // sau khi nhập input xong sẽ ẩn icon loading
+            })
+            .catch(() => {
+                // ẩn icon loading nếu có lỗi
+                setLoading(false);
+            });
+    }, [searchValue]);
 
     return (
         <HeadlessTippy // hiển thị bảng search bên dưới thẻ input
@@ -51,11 +68,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <Account />
-                        <Account />
-                        <Account />
-                        <Account />
-                        <Account />
+                        {searchResult.map((result) => (
+                            <Account key={result.id} data={result} /> // truyền data là kết quả lấy từ api
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -70,14 +85,15 @@ function Search() {
                     onChange={hanldeSearch}
                     onFocus={handleFocus} // khi focus thẻ input sẽ hiện lại bảng search
                 />
-                {!!searchValue && ( // convert searchValue sang kiểu boolean và nếu có searchValue thì mới render button clear
-                    <button className={cx('clear')} onClick={handleClear}>
-                        {/* <FontAwesomeIcon icon="fa-solid fa-circle-xmark" /> */}
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
-
+                {!!searchValue && // convert searchValue sang kiểu boolean
+                    !loading && ( // nếu có searchValue và state loading là false thì mới render button clear
+                        <button className={cx('clear')} onClick={handleClear}>
+                            {/* <FontAwesomeIcon icon="fa-solid fa-circle-xmark" /> */}
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+                {/* hiển thị icon loading khi state loading là true */}
                 <button className={cx('search-btn')}>
                     {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
                     <SearchIcon />
